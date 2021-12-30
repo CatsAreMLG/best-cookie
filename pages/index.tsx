b5d88c7f4ab0f546ad15133479ea32080301e962
CatsAreMLG <joshuadgon@gmail.com>
2021-12-29 23:53:16 -0600

    Add random cookie id fetcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
+import { getOptionsForVote } from "@utils/getRandomCookie"
 import { trpc } from "@utils/trpc"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Josh" }])
-
-  if (isLoading) return <div>Loading...</div>
-
-  if (data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote()
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Cookie is Better?</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200 text-center text-black">
+          {first}
+        </div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200 text-center text-black">
+          {second}
+        </div>
       </div>
     </div>
   )
---
+++ utils/getRandomCookie.ts
@@ -0,0 +1,17 @@
+const MAX_COOKIES_ID = 5
+
+export const getRandomCookie: (notThisOne?: number) => number = (
+  notThisOne?: number
+) => {
+  const cookieNumber = Math.floor(Math.random() * MAX_COOKIES_ID + 1)
+
+  if (cookieNumber !== notThisOne) return cookieNumber
+  return getRandomCookie(notThisOne)
+}
+
+export const getOptionsForVote = () => {
+  const firstId = getRandomCookie()
+  const secondId = getRandomCookie(firstId)
+
+  return [firstId, secondId]
+}
